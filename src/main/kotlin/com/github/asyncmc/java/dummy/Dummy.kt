e3ddf4c507119f09acd75ae7709d1ea695693eed
joserobjr <joserobjr@gamemods.com.br>
2020-05-31 21:59:21 -0300

    Configure the server boot

--- src/main/kotlin/com/github/asyncmc/java/dummy/Dummy.kt
+++ src/main/kotlin/com/github/asyncmc/java/dummy/Dummy.kt
@@ -1,4 +1,4 @@
-package dummy
+package com.github.asyncmc.java.dummy
 
 class Dummy {
     fun codecov(areYouWorking: String) {
--- src/test/kotlin/DummyTest.kt
+++ src/test/kotlin/DummyTest.kt
@@ -1,4 +1,4 @@
-import dummy.Dummy
+import com.github.asyncmc.java.dummy.Dummy
 import org.junit.jupiter.api.Test
 
 internal class DummyTest {
